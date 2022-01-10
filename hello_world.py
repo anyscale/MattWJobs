2fbec998a6d485c3f263e0b8961178b1f80ff0d3
Matt Weber <mattweber@anyscale.com>
2022-01-10 15:09:34 -0800

    reduce iterations

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,7 @@ import anyscale
 @ray.remote
 def say_hi(message):
     import time
-    for i in range(200):
+    for i in range(100):
         print(i)
         time.sleep(5)
     return f"Hi hi, {message}."
